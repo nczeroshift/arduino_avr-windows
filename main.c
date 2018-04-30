76eae454a0eb47fe39eb21e9f190d90ed4cadc9b
Luís F. Loureiro <nczeroshift.dev@gmail.com>
2018-04-30 11:20:04 +0100

    spaces and tabs

--- main.c
+++ main.c
@@ -14,15 +14,13 @@
 #include <util/setbaud.h>
 #include <compat/twi.h>
 
-int main(void)
-{
+int main(void){
     DDRB |= (1 << PB5);
-	while(1){
+    while(1){
         PORTB |= (1 << PB5);
         _delay_ms(500);
         PORTB &= ~(1 << PB5);
         _delay_ms(500);
-	}
-    
-	return 0;
+    }
+    return 0;
 }
